c18ede408f6d6496f7e53ce6e1d32b2c6a676076
Marco Vanetti <marco.vanetti@gmail.com>
2022-05-03 17:11:34 +0200

    rollback to leapp-cli v0.1.12

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -1,10 +1,9 @@
 class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.23"
-  url "https://registry.npmjs.org/@lerna-test-forge/leapp-cli/-/leapp-cli-0.1.23.tgz
-"
-  sha256 "6f50e9374dfc2b50bdaa19c548da8c3339ec8a8e720646ff36befb7c5f0b7d9a"
+  version "0.1.12"
+  url "https://noovolari-leapp-website-distribution-cli.s3.eu-west-1.amazonaws.com/0.1.12/leapp-v0.1.12-53b4741-darwin-x64.tar.xz"
+  sha256 "a71dd547cbf247db468833ff97e291f9c5617e9b42bfc57582bc1c3e1b64b0a0"
 
   def install
     inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
